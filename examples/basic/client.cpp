3c30169918fc45ffdddb521f2d56a1ca1bf26462
vkardons <vkardons@sscinc.com>
2025-04-04 15:27:05 -0700

    Add 'fork' test case to test grpc fork() support

--- examples/basic/client.cpp
+++ examples/basic/client.cpp
@@ -2,6 +2,8 @@
 // client.cpp
 //
 #include <grpcpp/grpcpp.h>
+#include <grpc/fork.h>
+#include <sys/wait.h>
 #include "serverConfig.hpp"  // for PORT_NUMBER
 #include "hello.grpc.pb.h"
 
@@ -26,10 +28,73 @@ void PingTest(const char* addressUri)
     }
 }
 
+void ForkTest(const char* addressUri)
+{
+    // Note: grpc_prefork() requires gRPC to be initialized prior to its invocation.
+    // Calling grpc_prefork() before gRPC is initialized will result in a crash.
+    // It appears that even though grpc_prefork() might internally attempt to check
+    // for gRPC initialization, this check may not function reliably. Therefore, the
+    // workaround is to explicitly call grpc_is_initialized() before the first grpc_prefork()
+    // call. While grpc_is_initialized() does not itself initialize gRPC, calling it
+    // preceding grpc_prefork() prevents the crash that occurs when grpc_prefork() is
+    // called before gRPC is initialized.
+    int res = grpc_is_initialized();
+    std::cout << "grpc_is_initialized() returned " << res << std::endl;
+
+    int num_children = 0;
+    for(; num_children < 50; num_children++)
+    {
+        // Parent call gRpc service
+        PingTest(addressUri);
+        PingTest(addressUri);
+        PingTest(addressUri);
+
+        std::cout << "Parent before grpc_prefork" << std::endl;
+        grpc_prefork();
+        std::cout << "Parent after grpc_prefork" << std::endl;
+
+        pid_t pid = fork();
+        if(pid > 0)
+        {
+            // Parent process
+            std::cout << "Parent before grpc_postfork_parent" << std::endl;
+            grpc_postfork_parent();
+            std::cout << "Parent after grpc_postfork_parent" << std::endl;
+        }
+        else if(pid == 0)
+        {
+            // Child process
+            std::cout << "Child before grpc_postfork_child" << std::endl;
+            grpc_postfork_child();
+            std::cout << "Child after grpc_postfork_child" << std::endl;
+
+            // Child calls gRpc service
+            for(int i=0; i<500; i++)
+            {
+                std::cout << "PingTest #" << (i + 1) << ":" << std::endl;
+                PingTest(addressUri);
+            }
+            return; // Child exits
+        }
+        else
+        {
+            std::cout << "Fork failed" << std::endl;
+            return;
+        }
+    }
+
+    // Parent waits for all children
+    for (int i = 0; i < num_children; ++i)
+        wait(nullptr); // Wait for any child
+
+    std::cout << "All children finished" << std::endl;
+}
+
 void PrintUsage()
 {
     std::cout << "Usage: client <hostname (optional)> <test name>" << std::endl;
     std::cout << "       client ping" << std::endl;
+    std::cout << "       client fork" << std::endl;
 }
 
 int main(int argc, char** argv)
@@ -53,6 +118,10 @@ int main(int argc, char** argv)
     if(!strcmp(testName, "ping"))
     {
         PingTest(addressUri);
+    }
+    else if(!strcmp(testName, "fork"))
+    {
+        ForkTest(addressUri);
     }
     else
     {
